2726c54f2db83d793da259898c8c3557d378f319
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-05-19 00:16:06 +0300

    Draft: dbContext

---
+++ back-end/BusinessService.DataLayer/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using BusinessService.DataLayer.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BusinessService.DataLayer
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<News> News { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            MigrateDatabase(this.Database.GetDbConnection());
+        }
+
+        private void MigrateDatabase(IDbConnection connection)
+        {
+           
+        }
+    }
+}

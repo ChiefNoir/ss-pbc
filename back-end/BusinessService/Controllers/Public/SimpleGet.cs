92da6aae83ebeb40209f8fa429fd90b8b1e1980f
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-05-22 00:30:00 +0300

    Draft of get controller

---
+++ back-end/BusinessService/Controllers/Public/SimpleGet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using BusinessService.Logic.Supervision;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessService.Controllers.Public
+{
+    public class SimpleGet : Controller
+    {
+        [HttpGet("news")]
+        public JsonResult GetNews()
+        {
+            var result = Supervisor.SafeExecute(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet("categories")]
+        public JsonResult GetCategory()
+        {
+            var result = Supervisor.SafeExecute(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet("categories/all")]
+        public JsonResult GetCategoryAll()
+        {
+            var result = Supervisor.SafeExecute(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet("projects/{start}/{length}/{categoryCode}")]
+        public JsonResult GetProjects(int start, int length, string categoryCode)
+        {
+            var result = Supervisor.SafeExecute(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            return new JsonResult(result);
+        }
+    }
+}

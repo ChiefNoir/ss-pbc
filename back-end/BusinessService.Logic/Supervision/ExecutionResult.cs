3fd0fecce0f3f3eed55285fca44d916c4b7525d4
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-05-21 23:11:52 +0300

    Added: class supervision of execute

---
+++ back-end/BusinessService.Logic/Supervision/ExecutionResult.cs
@@ -0,0 +1,16 @@
+﻿namespace BusinessService.Logic.Supervision
+{
+    /// <summary> Result of save execution</summary>
+    /// <typeparam name="T">Type</typeparam>
+    public class ExecutionResult<T>
+    {
+        /// <summary>Result </summary>
+        public T Data { get; internal set; } = default;
+
+        /// <summary> Is execution succeed?</summary>
+        public bool IsSucceed { get; internal set; } = false;
+
+        /// <summary> Error message or null </summary>
+        public string ErrorMessage { get; internal set; } = null;
+    }
+}
---
+++ back-end/BusinessService.Logic/Supervision/Supervisor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BusinessService.Logic.Supervision
+{
+    /// <summary> Utility class that provides wrapper for all things that might fail </summary>
+    /// <remarks> Service CAN NOT throw Exception and die</remarks>
+    public static class Supervisor
+    {
+        /// <summary> Execute function and return its result if anything goes right </summary>
+        /// <typeparam name="T">Type of returning result</typeparam>
+        /// <param name="func">Function to execute</param>
+        /// <returns>Result of execution or ErrorMessage will have message </returns>
+        public static ExecutionResult<T> SafeExecute<T>(Func<T> func)
+        {
+            var result = new ExecutionResult<T>();
+
+            try
+            {
+                result.Data = func();
+                result.IsSucceed = true;
+            }
+            catch (Exception ee)
+            {
+                result.IsSucceed = false;
+                result.ErrorMessage = ee.Message;
+            }
+
+            return result;
+        }
+
+        /// <summary> Execute function and return empty result if anything goes right </summary>
+        /// <param name="func">Function to execute</param>
+        /// <returns>Empty result of execution or ErrorMessage will have message </returns>
+        public static ExecutionResult<object> SafeExecute(Action func)
+        {
+            var result = new ExecutionResult<object>();
+
+            try
+            {
+                func();
+                result.IsSucceed = true;
+            }
+            catch (Exception ee)
+            {
+                result.IsSucceed = false;
+                result.ErrorMessage = ee.Message;
+            }
+
+            return result;
+        }
+    }
+}

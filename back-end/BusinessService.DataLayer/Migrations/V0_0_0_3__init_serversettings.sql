efe3083602d4b0d214c73030eedf75bd90496dfe
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-05-24 15:38:00 +0300

    Added: initial server settings

---
+++ back-end/BusinessService.DataLayer/Migrations/V0_0_0_3__init_serversettings.sql
@@ -0,0 +1,5 @@
+﻿insert into ServerSetting(Key, DisplayName, Value, version)
+	values('site_name', 'Site name', 'Beautiful site', 0);
+
+insert into ServerSetting(Key, DisplayName, Value, version)
+	values('projects_max_per_page', 'Maximum projects per page', '3', 0);

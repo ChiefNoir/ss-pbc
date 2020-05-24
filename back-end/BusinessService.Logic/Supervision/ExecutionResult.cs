e8778ce212900940715aacf060f19c0db0f3113d
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-05-24 22:29:39 +0300

    Clean up: Supervisor class

--- back-end/BusinessService.Logic/Supervision/ExecutionResult.cs
+++ back-end/BusinessService.Logic/Supervision/ExecutionResult.cs
@@ -1,16 +1,20 @@
 ﻿namespace BusinessService.Logic.Supervision
 {
-    /// <summary> Result of save execution</summary>
-    /// <typeparam name="T">Type</typeparam>
-    public class ExecutionResult<T>
+    /// <summary> Result of the function execution</summary>
+    public class ExecutionResult
     {
-        /// <summary>Result </summary>
-        public T Data { get; internal set; } = default;
-
         /// <summary> Is execution succeed?</summary>
         public bool IsSucceed { get; internal set; } = false;
 
         /// <summary> Error message or null </summary>
         public string ErrorMessage { get; internal set; } = null;
+    }
+
+    /// <summary> Result of the function execution</summary>
+    /// <typeparam name="T">Type of the result</typeparam>
+    public class ExecutionResult<T> : ExecutionResult
+    {
+        /// <summary>Result </summary>
+        public T Data { get; internal set; } = default;
     }
 }
--- back-end/BusinessService.Logic/Supervision/Supervisor.cs
+++ back-end/BusinessService.Logic/Supervision/Supervisor.cs
@@ -3,12 +3,12 @@
 namespace BusinessService.Logic.Supervision
 {
     /// <summary> Utility class that provides wrapper for all things that might fail </summary>
-    /// <remarks> Service CAN NOT throw Exception and die</remarks>
+    /// <remarks> Service <b>CAN NOT</b> throw <seealso cref="Exception"/> and die</remarks>
     public static class Supervisor
     {
-        /// <summary> Execute function and return its result if anything goes right </summary>
+        /// <summary> Execute function and return its result if everything goes right </summary>
         /// <typeparam name="T">Type of returning result</typeparam>
-        /// <param name="func">Function to execute</param>
+        /// <param name="func"><seealso cref="Func"/> to execute</param>
         /// <returns>Result of execution or ErrorMessage will have message </returns>
         public static ExecutionResult<T> SafeExecute<T>(Func<T> func)
         {
@@ -28,16 +28,16 @@ namespace BusinessService.Logic.Supervision
             return result;
         }
 
-        /// <summary> Execute function and return empty result if anything goes right </summary>
-        /// <param name="func">Function to execute</param>
-        /// <returns>Empty result of execution or ErrorMessage will have message </returns>
-        public static ExecutionResult<object> SafeExecute(Action func)
+        /// <summary> Execute function </summary>
+        /// <param name="action"><seealso cref="Action"/> to execute</param>
+        /// <returns>Result of execution or ErrorMessage will have message </returns>
+        public static ExecutionResult SafeExecute(Action action)
         {
-            var result = new ExecutionResult<object>();
+            var result = new ExecutionResult();
 
             try
             {
-                func();
+                action();
                 result.IsSucceed = true;
             }
             catch (Exception ee)
@@ -48,5 +48,6 @@ namespace BusinessService.Logic.Supervision
 
             return result;
         }
+
     }
 }

6f808e4977b87472eb25380cc558e1c8c0a54012
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-05-25 02:27:55 +0300

    [front] Added: footer

--- front-end/BusinessSite/src/app/app.module.ts
+++ front-end/BusinessSite/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

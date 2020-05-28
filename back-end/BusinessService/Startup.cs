cf68216164e6f729ccf971d68177a19b2deafb64
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-05-28 22:36:08 +0300

    [back-end] Allow cors

--- back-end/BusinessService/Startup.cs
+++ back-end/BusinessService/Startup.cs
@@ -42,6 +42,10 @@ namespace BusinessService
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
 
             app.UseAuthorization();
 

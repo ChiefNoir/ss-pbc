57e379ef0bba303a22638941c5c62dba2289ee70
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-05-29 07:49:37 +0300

    [front] Added getProjects in dataService

--- front-end/BusinessSite/src/app/service/data.service.ts
+++ front-end/BusinessSite/src/app/service/data.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { RequestResult } from '../model/RequestResult';
+import { Project } from '../model/Project';
+
 import { News } from '../model/News';
 
 import { Injectable } from '@angular/core';
@@ -20,6 +22,13 @@ export class DataService {
         return this.httpClient.get<RequestResult<Array<News>>>
         (
           this.endpoint + 'news/all'
+        );
+      }
+
+      public getProjects(start: number, length: number, categoryCode: string): Observable<RequestResult<Array<Project>>> {
+        return this.httpClient.get<RequestResult<Array<Project>>>
+        (
+          this.endpoint + 'projects/' + categoryCode + '/' + start + '/' + length + '/'
         );
       }
 }

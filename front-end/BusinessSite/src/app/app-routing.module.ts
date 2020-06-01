64c908df0b66184f954e013c83e6cc3c2a7d5949
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-06-01 13:19:21 +0300

    [front-end] Added: projects-list view draft

--- front-end/BusinessSite/src/app/app-routing.module.ts
+++ front-end/BusinessSite/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './view/home/home.component';
+import { ProjectsListComponent } from './view/projects-list/projects-list.component';
 import { NotFoundComponent } from './view/notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'projects', component: ProjectsListComponent },
   { path: '**', component: NotFoundComponent }
 ];
 

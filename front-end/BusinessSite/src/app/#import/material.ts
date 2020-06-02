780100c8af9565e5151ae981583e8facb77a2ddc
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-06-03 01:47:12 +0300

    [front] Added MatDivider

--- front-end/BusinessSite/src/app/#import/material.ts
+++ front-end/BusinessSite/src/app/#import/material.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 const Material =
 [
   MatSidenavModule,
   MatToolbarModule,
-  MatButtonModule
+  MatButtonModule,
+  MatDividerModule
 ];
 
 @NgModule({

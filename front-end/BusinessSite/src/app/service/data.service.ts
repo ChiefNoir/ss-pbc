c8db1eeb532cde7afee6647372c924ecd69019a1
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-06-07 23:33:08 +0300

    [front-end] Cleanup: dataService

--- front-end/BusinessSite/src/app/service/data.service.ts
+++ front-end/BusinessSite/src/app/service/data.service.ts
@@ -1,50 +1,52 @@
 import { HttpClient } from '@angular/common/http';
-import { RequestResult } from '../model/RequestResult';
+import { Injectable } from '@angular/core';
 
+import { RequestResult } from '../model/RequestResult';
 import { Project } from '../model/Project';
 import { News } from '../model/News';
 import { Category } from '../model/Category';
 
-import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-
 
 @Injectable()
 export class DataService {
-    private httpClient: HttpClient;
-    private endpoint = environment.apiEndpoint;
+  private httpClient: HttpClient;
+  private endpoint = environment.apiEndpoint;
 
-    public constructor(http: HttpClient) {
-        this.httpClient = http;
-      }
+  public constructor(http: HttpClient) {
+    this.httpClient = http;
+  }
 
-      public getNews(): Promise<RequestResult<Array<News>>> {
-        return this.httpClient.get<RequestResult<Array<News>>>
-        (
-          this.endpoint + 'news/all'
-        ).toPromise();
-      }
+  public getNews(): Promise<RequestResult<Array<News>>> {
+    return this.httpClient
+               .get<RequestResult<Array<News>>>(this.endpoint + 'news/all')
+               .toPromise();
+  }
 
-      public getProjects(start: number, length: number, categoryCode: string): Promise<RequestResult<Array<Project>>> {
-        return this.httpClient.get<RequestResult<Array<Project>>>
-        (
-          this.endpoint + 'projects/' + categoryCode + '/' + start + '/' + length
-        ).toPromise();
-      }
+  public getProjects(start: number, length: number, categoryCode: string): Promise<RequestResult<Array<Project>>> {
+    return this.httpClient
+               .get<RequestResult<Array<Project>>>
+               (
+                 this.endpoint + 'projects/' + categoryCode + '/' + start + '/' + length
+               )
+               .toPromise();
+  }
 
-      public getProject(code: string): Promise<RequestResult<Project>> {
-        return this.httpClient.get<RequestResult<Project>>
-        (
-          this.endpoint + 'project/' + code
-        ).toPromise();
-      }
+  public getProject(code: string): Promise<RequestResult<Project>> {
+    return this.httpClient
+               .get<RequestResult<Project>>
+               (
+                 this.endpoint + 'project/' + code
+               )
+               .toPromise();
+  }
 
-      public getCategories(): Promise<RequestResult<Array<Category>>>
-      {
-        return this.httpClient.get<RequestResult<Array<Category>>>
-        (
-          this.endpoint + 'categories/all'
-        ).toPromise();
-      }
-
+  public getCategories(): Promise<RequestResult<Array<Category>>> {
+    return this.httpClient
+               .get<RequestResult<Array<Category>>>
+               (
+                 this.endpoint + 'categories/all'
+               )
+               .toPromise();
+  }
 }

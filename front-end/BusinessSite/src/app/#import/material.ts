4f695632a85b64a24bf6372baf79c0ca962b7e52
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-06-07 23:30:11 +0300

    [front-end] Cleanup: material imports

--- front-end/BusinessSite/src/app/#import/material.ts
+++ front-end/BusinessSite/src/app/#import/material.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 
-const Material =
-[
+const Material = [
   MatSidenavModule,
   MatToolbarModule,
   MatButtonModule,
-  MatDividerModule
+  MatDividerModule,
 ];
 
 @NgModule({
-    imports: Material,
-    exports: Material
-  })
-  export class MaterialModules {}
+  imports: Material,
+  exports: Material,
+})
+
+export class MaterialModules {}

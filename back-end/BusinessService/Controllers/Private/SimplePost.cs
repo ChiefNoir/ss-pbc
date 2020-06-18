8b45c978f3eddf2a24e7f5cd6444ff9bcc778bc6
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-06-19 00:29:19 +0300

    [back-end] Added: SaveCategory to the API

---
+++ back-end/BusinessService/Controllers/Private/SimplePost.cs
@@ -0,0 +1,34 @@
+﻿using Abstractions.IRepository;
+using Abstractions.Model;
+using API.Queries;
+using BusinessService.Logic.Supervision;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers.Private
+{
+    [ApiController]
+    [Route("api/v1/")]
+    public class SimplePost : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public SimplePost(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+
+        [HttpPost("category")]
+        public async Task<IActionResult> Save([FromBody] Category category)
+        {
+            var result = await Supervisor.SafeExecuteAsync(() =>
+            {
+                return _categoryRepository.SaveCategory(category);
+            });
+
+            return new JsonResult(result);
+        }
+
+    }
+}

c77f93b5bb893d4af810056c28e2ef152b2af29f
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-06-26 01:17:49 +0300

    [back-end] Draft: Files repository

---
+++ back-end/Abstractions/IRepository/IFileRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Abstractions.IRepository
+{
+    public interface IFileRepository
+    {
+        /// <summary> Save <see cref="IFormFile"/></summary>
+        /// <param name="file">File to save</param>
+        /// <returns>Relative path to file</returns>
+        Task<string> Save(IFormFile file);
+    }
+}
---
+++ back-end/Infrastructure/Repository/FileRepository.cs
@@ -0,0 +1,50 @@
+﻿using Abstractions.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class FileRepository : IFileRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public FileRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<string> Save(IFormFile file)
+        {
+            if (file == null || file.Length == 0) //TODO: maxfilesize
+                throw new Exception("Empty file"); //TODO: custom exception
+
+            var folderName = _configuration.GetSection("Location").GetValue<string>("FileStorage");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+
+            var filename = GenerateFileName() + Path.GetExtension(file.FileName);
+
+            var fullPath = Path.Combine(pathToSave, filename);
+            var dbPath = Path.Combine(folderName, filename);
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return filename;
+        }
+
+
+        private static string GenerateFileName()
+        {
+            //TODO: temporary solution
+            return DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds.ToString();
+        }
+    }
+}

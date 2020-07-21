9db582ebbc440e40f3dce6ef6cb5012b6802d4f4
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-07-21 17:02:34 +0300

    [back-end] Added Introduction model to the abstractions

---
+++ back-end/Abstractions/Model/Introduction.cs
@@ -0,0 +1,18 @@
+﻿using Abstractions.Common;
+using System.Collections.Generic;
+
+namespace Abstractions.Model
+{
+    public class Introduction : IVersion
+    {        
+        public string Content { get; set; }
+
+        public string PosterUrl { get; set; }
+
+        public string PosterDescription { get; set; }
+
+        public IEnumerable<ExternalUrl> ExternalUrls { get; set; }
+
+        public long Version { get; set; }
+    }
+}

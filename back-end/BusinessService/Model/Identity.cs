6658ae10fef6e99e1016512b2bba18337ebd5582
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-07-28 20:20:31 +0300

    [back-end] API models cleanup

--- back-end/BusinessService/Model/Identity.cs
+++ back-end/BusinessService/Model/Identity.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace API.Model
+﻿namespace API.Model
 {
-    class Identity
+    /// <summary> Account confirmed identity</summary>
+    internal class Identity
     {
+        /// <summary> Account login </summary>
         public string Login { get; set; }
+
+        /// <summary> Account identification  token </summary>
         public string Token { get; set; }
+
+        /// <summary> Tokens lifespan </summary>
         public int TokenLifeTimeMinutes { get; set; }
     }
 }

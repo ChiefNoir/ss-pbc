213a9829bd0162a06f214bd2bb3490d20ea2dbe3
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-07-28 21:32:31 +0300

    [migrations] cleanup

--- back-end/Infrastructure/Migrations/V0_0_0_3__init_view.sql
+++ back-end/Infrastructure/Migrations/V0_0_0_3__init_view.sql
@@ -1,22 +1,21 @@
 ﻿CREATE VIEW categories_with_projects_total_v AS
 (
 	select 
-	c.*, coalesce(sm.total, 0) as total_projects
-from 
-	site.category c 
-left join
-	(
-		select
-			coalesce(category_id, (select id from site.category where is_everything = true limit 1) ) as id, 
-			count(1) as total
-		from 
-			site.project 
-		group by
-			rollup(category_id)
-	) sm 
-on 
-	c.id = sm.id
-order by 
-	total desc
-
+		c.*, coalesce(sm.total, 0) as total_projects
+	from 
+		site.category c
+		left join
+		(
+			select
+				coalesce(category_id, (select id from site.category where is_everything = true limit 1) ) as id, 
+				count(1) as total
+			from 
+				site.project 
+			group by
+				rollup(category_id)
+		) sm 
+	on 
+		c.id = sm.id
+	order by 
+		total desc
 );

26fbc1b4256170620d010201dd67eb4c197813ae
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-07-28 20:22:09 +0300

    [back-end] TokenManager cleanup

--- back-end/BusinessService/Security/TokenManager.cs
+++ back-end/BusinessService/Security/TokenManager.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
@@ -13,6 +12,39 @@ namespace API.Security
     /// <summary> JWT Toke manager </summary>
     public static class TokenManager
     {
+        /// <summary> Create JWT token </summary>
+        /// <param name="configuration"> <see cref="IConfiguration"/> </param>
+        /// <param name="login"> Login </param>
+        /// <param name="roles"> Roles </param>
+        /// <returns>JWT token</returns>
+        public static string CreateToken(IConfiguration configuration, string login, params string[] roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, login)
+            };
+            foreach (var item in roles)
+            {
+                claims.Add(new Claim(ClaimsIdentity.DefaultRoleClaimType, item));
+            }
+
+            var jwt = new JwtSecurityToken(
+                    issuer: configuration.GetSection("Token").GetValue<string>("Issuer"),
+                    audience: configuration.GetSection("Token").GetValue<string>("Audience"),
+                    notBefore: DateTime.UtcNow,
+                    claims: claims,
+                    expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(configuration.GetSection("Token").GetValue<int>("LifeTime"))),
+                    signingCredentials: new SigningCredentials
+                    (
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("Token").GetValue<string>("Key"))),
+                        configuration.GetSection("Token").GetValue<string>("SecurityAlgorithms")
+                    ));
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return encodedJwt;
+        }
+
         /// <summary> Create <seea cref="TokenValidationParameters"/> parameters </summary>
         /// <param name="configuration"> <see cref="IConfiguration"/> </param>
         /// <returns> <seea cref="TokenValidationParameters"/> </returns>
@@ -30,40 +62,7 @@ namespace API.Security
                 RequireExpirationTime = true
             };
         }
-
-        /// <summary> Create JWT token </summary>
-        /// <param name="configuration"> <see cref="IConfiguration"/> </param>
-        /// <param name="login"> Login </param>
-        /// <param name="roles"> Roles </param>
-        /// <returns>JWT token</returns>
-        public static string CreateToken(IConfiguration configuration, string login, params string[] roles)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimsIdentity.DefaultNameClaimType, login)
-            };
-            foreach (var item in roles)
-            {
-                claims.Add(new Claim(ClaimsIdentity.DefaultRoleClaimType, item));
-            }
-            
-            var jwt = new JwtSecurityToken(
-                    issuer: configuration.GetSection("Token").GetValue<string>("Issuer"),
-                    audience: configuration.GetSection("Token").GetValue<string>("Audience"),
-                    notBefore: DateTime.UtcNow,
-                    claims: claims,
-                    expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(configuration.GetSection("Token").GetValue<int>("LifeTime"))),
-                    signingCredentials: new SigningCredentials
-                    (
-                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("Token").GetValue<string>("Key"))),
-                        configuration.GetSection("Token").GetValue<string>("SecurityAlgorithms")
-                    ));
-
-            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
-            
-            return encodedJwt;
-        }
-
+        
         /// <summary> Validate JWT token </summary>
         /// <param name="configuration"> <see cref="IConfiguration"/> </param>
         /// <param name="token">Token to validate</param>

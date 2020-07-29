c7ea25d98b75a3e75723b6d4477c9a9b55d3676e
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-07-30 01:49:44 +0300

    [front-end] Fixed wrong text in static names

--- front-end/BusinessSite/src/app/common/StaticNames.ts
+++ front-end/BusinessSite/src/app/common/StaticNames.ts
@@ -4,7 +4,7 @@ export class StaticNames
   public static LoadComplete = 'Load complete';
 
   public static SaveComplete = 'Saving complete';
-  public static SaveInProgress = 'Delete in progress';
+  public static SaveInProgress = 'Saving in progress';
 
   public static DeleteComplete = 'Deleting complete';
   public static DeleteInProgress = 'Delete in progress';

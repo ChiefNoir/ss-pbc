06600059eac88a4a3433c4bf7149fe7eaf0f7667
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-08-03 23:10:09 +0300

    [front-end] Fixed urls in FilterCategoryComponent

--- front-end/BusinessSite/src/app/component/filter-category/filter-category.component.ts
+++ front-end/BusinessSite/src/app/component/filter-category/filter-category.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Category } from 'src/app/model/Category';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-filter-category',
@@ -7,8 +8,31 @@ import { Category } from 'src/app/model/Category';
   styleUrls: ['./filter-category.component.scss']
 })
 
-export class FilterCategoryComponent
+export class FilterCategoryComponent implements OnChanges
 {
   @Input()
   public categories: Array<Category>;
+
+  private router: Router;
+
+  constructor(router: Router)
+  {
+    this.router = router;
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void
+  {
+    if (changes.categories.currentValue)
+    {
+      changes.categories
+             .currentValue.forEach
+             (
+               x => { x.url = this.router.createUrlTree(['/projects', x.code]).toString(); }
+             );
+
+      this.categories = changes.categories.currentValue
+                                          .filter(x => x.totalProjects > 0)
+                                          .sort((a, b) => b.totalProjects - a.totalProjects);
+    }
+  }
 }

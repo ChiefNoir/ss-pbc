21cfe5f398e9bccb9b7e212e0b2791a948d3fb23
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-08-14 00:44:24 +0300

    [back-end] Refactoring: replaced Login with Account class

--- back-end/BusinessService/Model/Identity.cs
+++ back-end/BusinessService/Model/Identity.cs
@@ -1,10 +1,11 @@
-﻿namespace API.Model
+﻿using Abstractions.Model;
+
+namespace API.Model
 {
     /// <summary> Account confirmed identity</summary>
     internal class Identity
     {
-        /// <summary> Account login </summary>
-        public string Login { get; set; }
+        public Account Account { get; set; }
 
         /// <summary> Account identification  token </summary>
         public string Token { get; set; }

ec7409f9bc73f0426b9fc17065ecef481bee9b99
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-08-14 00:46:06 +0300

    [back-end] Added token information API

---
+++ back-end/BusinessService/Controllers/Private/PrivateInformationalController.cs
@@ -0,0 +1,49 @@
+﻿using Abstractions.IRepository;
+using Abstractions.Model;
+using API.Model;
+using API.Queries;
+using API.Security;
+using BusinessService.Logic.Supervision;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace API.Controllers.Private
+{
+    [ApiController]
+    [Route("api/v1/")]
+    public class PrivateInformationalController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IConfiguration _config;
+
+        public PrivateInformationalController(IAccountRepository accountRepository, IConfiguration config)
+        {
+            _accountRepository = accountRepository;
+            _config = config;
+        }
+
+        [HttpGet("information")]
+        public async Task<IActionResult> GetIntroduction([FromHeader] string token)
+        {
+            var result = await Supervisor.SafeExecuteAsync(async () =>
+            {
+                var claims = TokenManager.ValidateToken(_config, token);
+
+                if (claims == null)
+                    throw new Exception("No");
+
+                return new Information
+                {
+                    Login = claims.Identity.Name,
+                    Role = "TODO",
+                    APIVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString()
+                };
+            });
+
+            return new JsonResult(result);
+        }
+    }
+}

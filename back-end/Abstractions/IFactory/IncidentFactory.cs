6dcceae75df8a3a9131ec047b412079df48d883d
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-09-14 22:11:09 +0300

    [back-end] Resolved #9: Refactoring. IncidentFactory

--- back-end/Abstractions/IFactory/IncidentFactory.cs
+++ back-end/Abstractions/IFactory/IncidentFactory.cs
@@ -3,48 +3,55 @@ using System;
 
 namespace Abstractions.IFactory
 {
+    /// <summary> Exceptional situation messages factory </summary>
     public static class IncidentFactory
     {
+        /// <summary> Create human-friendly exceptional situation message </summary>
+        /// <param name="code"> Exceptional situation code </param>
+        /// <param name="details"> Additional message </param>
+        /// <returns> <see cref="Incident"/> </returns>
         public static Incident Create(IncidentsCodes code, string details = null)
         {
-            var result = new Incident(code);
-            result.Detail = details;
+            var result = new Incident(code)
+            {
+                Detail = details
+            };
 
             switch (code)
             {
                 case IncidentsCodes.AuthenticationNotProvided:
                     {
-                        result.Message = "AuthenticationNotProvided";
+                        result.Message = "Authentication not provided";
                         break;
                     }
                 case IncidentsCodes.AuthenticationFailed:
                     {
-                        result.Message = "AuthenticationFailed";
+                        result.Message = "Authentication failed";
                         break;
                     }
                 case IncidentsCodes.AccessDenied:
                     {
-                        result.Message = "AccessDenied";
+                        result.Message = "Access denied";
                         break;
                     }
                 case IncidentsCodes.InternalError:
                     {
-                        result.Message = "InternalError";
+                        result.Message = "Internal error";
                         break;
                     }
                 case IncidentsCodes.VersionInconsistency:
                     {
-                        result.Message = "VersionInconsistency";
+                        result.Message = "The entry has been already changed";
                         break;
                     }
                 case IncidentsCodes.InvalidToken:
                     {
-                        result.Message = "InvalidToken";
+                        result.Message = "Invalid token";
                         break;
                     }
                 default:
                     {
-                        throw new Exception($"{code} is unknown Incident code");
+                        throw new Exception($"{code} is unknown incident code");
                     }
             }
 

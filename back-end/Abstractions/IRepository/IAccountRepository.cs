adf8d5049bad33abe369cfc5167bf9f99299470e
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-09-15 00:16:15 +0300

    [back-end] Resolved #9: Refactoring. AccountRepository

--- back-end/Abstractions/IRepository/IAccountRepository.cs
+++ back-end/Abstractions/IRepository/IAccountRepository.cs
@@ -6,21 +6,16 @@ namespace Abstractions.IRepository
     /// <summary> Account repository </summary>
     public interface IAccountRepository
     {
-        /// <summary>Add new <see cref="Account"/> to the storage </summary>
-        /// <param name="account">New account</param>
-        /// <returns>Created <see cref="Account"/> </returns>
-        Task<Account> AddAsync(Account account);
-
-        /// <summary> Count <see cref="Account"/> in the storage</summary>
-        /// <returns> Total of the accounts </returns>
+        /// <summary> Count items in the storage</summary>
+        /// <returns> Total of the items in the storage</returns>
         Task<int> CountAsync();
 
-        /// <summary> Delete <see cref="Account"/>  from storage </summary>
-        /// <param name="account"><see cref="Account"/> to delete</param>
+        /// <summary> Delete item from storage </summary>
+        /// <param name="item"><see cref="Account"/> to delete</param>
         /// <returns> <c>true</c> if deleting was successful </returns>
-        Task<bool> DeleteAsync(Account account);
+        Task<bool> DeleteAsync(Account item);
 
-        /// <summary>  Get existing <see cref="Account"/> </summary>
+        /// <summary> Get existing <see cref="Account"/> </summary>
         /// <param name="id">Account id</param>
         /// <returns>Existing <see cref="Account"/> or <see cref="null"/> </returns>
         Task<Account> GetAsync(int id);
@@ -31,16 +26,15 @@ namespace Abstractions.IRepository
         /// <returns>Existing <see cref="Account"/> or <see cref="null"/> </returns>
         Task<Account> GetAsync(string login, string plainTextPassword);
 
-        
+        /// <summary>Save <see cref="Account"/> to the storage </summary>
+        /// <param name="item">New or updated account</param>
+        /// <returns>Created or updated <see cref="Account"/> </returns>
+        Task<Account> SaveAsync(Account item);
+
         /// <summary> Search within <see cref="Account"/>, with paging </summary>
         /// <param name="start">Start</param>
         /// <param name="length">Length</param>
-        /// <returns> Array of <see cref="Account"/>, containing keyword </returns>
+        /// <returns> Array of <see cref="Account"/> </returns>
         Task<Account[]> SearchAsync(int start, int length);
-
-        /// <summary> Update existing <see cref="Account"/> </summary>
-        /// <param name="account"> Edited <see cref="Account"/></param>
-        /// <returns>Updated <see cref="Account"/> </returns>
-        Task<Account> UpdateAsync(Account account);
     }
 }

d28ac50866c58980df3c9c2c5f9044edfa41d41c
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-09-15 19:57:39 +0300

    [front-end] Resolved #10: Cleanup routing

--- front-end/BusinessSite/src/app/app-routing.module.ts
+++ front-end/BusinessSite/src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IntroductionComponent } from './view/introduction/introduction.component';
-import { ProjectsListComponent } from './view/projects-list/projects-list.component';
-import { ProjectComponent } from './view/project/project.component';
-import { NotFoundComponent } from './view/notfound/notfound.component';
-import { AdminLoginComponent } from './view/admin-login/admin-login.component';
-import { AdminEditProjectsComponent } from './view/admin-edit-projects/admin-edit-projects.component';
-import { AdminEditCategoriesComponent } from './view/admin-edit-categories/admin-edit-categories.component';
-import { AdminEditAccountsComponent } from './view/admin-edit-accounts/admin-edit-accounts.component';
-import { AdminEditIntroductionComponent } from './view/admin-edit-introduction/admin-edit-introduction.component';
-import { AuthGuard } from './guards/authGuard';
 import { AdminComponent } from './view/admin/admin.component';
+import { AdminEditAccountsComponent } from './view/admin-edit-accounts/admin-edit-accounts.component';
+import { AdminEditCategoriesComponent } from './view/admin-edit-categories/admin-edit-categories.component';
+import { AdminEditIntroductionComponent } from './view/admin-edit-introduction/admin-edit-introduction.component';
+import { AdminEditProjectsComponent } from './view/admin-edit-projects/admin-edit-projects.component';
+import { AdminLoginComponent } from './view/admin-login/admin-login.component';
+import { IntroductionComponent } from './view/introduction/introduction.component';
+import { NotFoundComponent } from './view/notfound/notfound.component';
+import { ProjectComponent } from './view/project/project.component';
+import { ProjectsListComponent } from './view/projects-list/projects-list.component';
 
-const routes: Routes = [
+import { AuthGuard } from './guards/authGuard';
+
+const routes: Routes =
+[
   { path: '', component: IntroductionComponent, pathMatch: 'full' },
   { path: 'projects', component: ProjectsListComponent },
   { path: 'login', component: AdminLoginComponent },

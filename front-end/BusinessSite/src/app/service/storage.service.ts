378c00131ed47eb86797e2b5c3741cb6eb749fb6
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-09-15 19:23:54 +0300

    [front-end] Resolved #10: Cleanup: StorageService

--- front-end/BusinessSite/src/app/service/storage.service.ts
+++ front-end/BusinessSite/src/app/service/storage.service.ts
@@ -1,14 +1,15 @@
-import { CookieService } from 'ngx-cookie-service';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
-export class StorageService {
+export class StorageService
+{
   private readonly tokenName = window.location.hostname + '_token';
   private readonly path = window.location.host;
-
   private cookieService: CookieService;
 
-  public constructor(cookieService: CookieService) {
+  public constructor(cookieService: CookieService)
+  {
     this.cookieService = cookieService;
   }
 
@@ -17,7 +18,8 @@ export class StorageService {
     return this.cookieService.get(this.tokenName);
   }
 
-  public saveToken(value: string, expires: number): void {
+  public saveToken(value: string, expires: number): void
+  {
     this.cookieService.set
         (
           this.tokenName,
@@ -30,7 +32,8 @@ export class StorageService {
         );
   }
 
-  public removeToken(): void {
+  public removeToken(): void
+  {
     this.cookieService.delete(this.tokenName);
   }
 }

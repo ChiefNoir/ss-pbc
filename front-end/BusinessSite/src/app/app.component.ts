2db89cd509daa17c219c311fe416ea3926519af2
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-09-17 19:35:18 +0300

    [front-end] Resolved #10: Moved string literals from template to the dedicated class

--- front-end/BusinessSite/src/app/app.component.ts
+++ front-end/BusinessSite/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { AuthGuard } from './guards/authGuard';
+import { StaticNames } from './common/StaticNames';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ export class AppComponent
 {
   public authGuard: AuthGuard;
   public router: Router;
+  public staticNames = StaticNames;
 
   public constructor(titleService: Title, authGuard: AuthGuard, router: Router)
   {
--- front-end/BusinessSite/src/app/common/StaticNames.ts
+++ front-end/BusinessSite/src/app/common/StaticNames.ts
@@ -19,4 +19,12 @@ export class StaticNames
   public static TitlePageLogin = 'Login' + StaticNames.TitleSeparator;
 
   public static IntroductionExternalUrls = 'Meet me on the other site';
+
+  public static MenuHome = 'Home';
+  public static MenuProjects = 'Projects';
+  public static MenuAdmin = 'Admin';
+  public static MenuLogin = 'Login';
+  public static MenuCategories = 'categories';
+  public static MenuAccounts = 'accounts';
+  public static MenuIntroduction = 'introduction';
 }

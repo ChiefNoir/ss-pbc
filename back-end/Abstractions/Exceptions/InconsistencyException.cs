f01f8815deb199b6deda669ee86565d13b42e51a
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-09-18 16:48:50 +0300

    [back-end] Link #34: Added custom Exceptions

---
+++ back-end/Abstractions/Exceptions/InconsistencyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstractions.Exceptions
+{
+    /// <summary> Represents the potential inconsistency error in the storage </summary>
+    public class InconsistencyException : Exception
+    {
+        public InconsistencyException(string message, Exception innerException = null):base(message, innerException)
+        {
+        }
+    }
+}
---
+++ back-end/Abstractions/Exceptions/SecurityException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.Exceptions
+{
+    /// <summary> Represents the security error </summary>
+    public class SecurityException : Exception
+    {
+        public SecurityException(string message, Exception innerException = null) : base(message, innerException)
+        {
+        }
+    }
+}

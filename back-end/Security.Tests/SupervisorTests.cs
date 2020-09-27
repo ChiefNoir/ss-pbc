ed6b4cc70ef6a186c11ecead30e5ccaab5e66afd
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-09-28 00:12:30 +0300

    Update SupervisorTests.cs

--- back-end/Security.Tests/SupervisorTests.cs
+++ back-end/Security.Tests/SupervisorTests.cs
@@ -12,30 +12,30 @@ namespace Security.Tests
 {
     public class SupervisorTests
     {
-        private readonly ISupervisor supervisor;
-        private readonly Mock<IConfiguration> config = new Mock<IConfiguration>();
-        private readonly Mock<ILogRepository> log = new Mock<ILogRepository>();
+        private readonly ISupervisor _supervisor;
+        private readonly Mock<IConfiguration> _config = new Mock<IConfiguration>();
+        private readonly Mock<ILogRepository> _log = new Mock<ILogRepository>();
 
         public SupervisorTests()
         {
-            supervisor = new Supervisor(config.Object, log.Object);
+            _supervisor = new Supervisor(_config.Object, _log.Object);
         }
 
 
         [Fact]
         public void SafeExecute_Valid()
         {
-            var resultString = supervisor.SafeExecute(() => { return "text"; } );
+            var resultString = _supervisor.SafeExecute(() => { return "text"; } );
             Assert.True(resultString.Data == "text");
             Assert.True(resultString.IsSucceed);
             Assert.True(resultString.Error == null);
 
-            var resultInt = supervisor.SafeExecute(() => { return 12; });
+            var resultInt = _supervisor.SafeExecute(() => { return 12; });
             Assert.True(resultInt.Data == 12);
             Assert.True(resultInt.IsSucceed);
             Assert.True(resultInt.Error == null);
 
-            var resultCollection = supervisor.SafeExecute(() => { return new List<int> { 42 } ; });
+            var resultCollection = _supervisor.SafeExecute(() => { return new List<int> { 42 } ; });
             Assert.True(resultCollection.Data.Count == 1);
             Assert.True(resultCollection.Data[0] == 42);
             Assert.True(resultCollection.IsSucceed);
@@ -46,21 +46,21 @@ namespace Security.Tests
         [Fact]
         public void SafeExecute_Invalid()
         {
-            var resultString = supervisor.SafeExecute<string>(() => { throw new Exception("One"); });
+            var resultString = _supervisor.SafeExecute<string>(() => { throw new Exception("One"); });
             Assert.True(resultString.Data == default);
             Assert.True(resultString.IsSucceed == false);
             Assert.True(resultString.Error != null);
             Assert.True(resultString.Error.Message == "One");
             Assert.True(resultString.Error.Detail == null);
 
-            var resultInt = supervisor.SafeExecute<int>(() => { throw new Exception("Two"); });
+            var resultInt = _supervisor.SafeExecute<int>(() => { throw new Exception("Two"); });
             Assert.True(resultInt.Data == default);
             Assert.True(resultInt.IsSucceed == false);
             Assert.True(resultInt.Error != null);
             Assert.True(resultInt.Error.Message == "Two");
             Assert.True(resultInt.Error.Detail == null);
 
-            var resultCollection = supervisor.SafeExecute<List<int>>(() => { throw new Exception("Three"); });
+            var resultCollection = _supervisor.SafeExecute<List<int>>(() => { throw new Exception("Three"); });
             Assert.True(resultCollection.Data == default);
             Assert.True(resultCollection.IsSucceed == false);
             Assert.True(resultCollection.Error != null);
@@ -68,7 +68,7 @@ namespace Security.Tests
             Assert.True(resultCollection.Error.Detail == null);
 
 
-            var result = supervisor.SafeExecute<int>(() => { throw new Exception("Main", new Exception("Inner")); });
+            var result = _supervisor.SafeExecute<int>(() => { throw new Exception("Main", new Exception("Inner")); });
             Assert.True(result.Data == default);
             Assert.True(result.IsSucceed == false);
             Assert.True(result.Error != null);
@@ -80,17 +80,17 @@ namespace Security.Tests
         [Fact]
         public async void SafeExecuteAsync_Valid()
         {
-            var resultString = await supervisor.SafeExecuteAsync(() => { return Task.FromResult("text"); });
+            var resultString = await _supervisor.SafeExecuteAsync(() => { return Task.FromResult("text"); });
             Assert.True(resultString.Data == "text");
             Assert.True(resultString.IsSucceed);
             Assert.True(resultString.Error == null);
 
-            var resultInt = await supervisor.SafeExecuteAsync(() => { return Task.FromResult(12); });
+            var resultInt = await _supervisor.SafeExecuteAsync(() => { return Task.FromResult(12); });
             Assert.True(resultInt.Data == 12);
             Assert.True(resultInt.IsSucceed);
             Assert.True(resultInt.Error == null);
 
-            var resultCollection  = await supervisor.SafeExecuteAsync(() => { return Task.FromResult(new List<int> { 42 }); });
+            var resultCollection  = await _supervisor.SafeExecuteAsync(() => { return Task.FromResult(new List<int> { 42 }); });
             Assert.True(resultCollection.Data.Count == 1);
             Assert.True(resultCollection.Data[0] == 42);
             Assert.True(resultCollection.IsSucceed);
@@ -101,21 +101,21 @@ namespace Security.Tests
         [Fact]
         public async void SafeExecuteAsync_Invalid()
         {
-            var resultString = await supervisor.SafeExecuteAsync<string>(() => { throw new Exception("One"); });
+            var resultString = await _supervisor.SafeExecuteAsync<string>(() => { throw new Exception("One"); });
             Assert.True(resultString.Data == default);
             Assert.True(resultString.IsSucceed == false);
             Assert.True(resultString.Error != null);
             Assert.True(resultString.Error.Message == "One");
             Assert.True(resultString.Error.Detail == null);
 
-            var resultInt = await supervisor.SafeExecuteAsync<int>(() => { throw new Exception("Two"); });
+            var resultInt = await _supervisor.SafeExecuteAsync<int>(() => { throw new Exception("Two"); });
             Assert.True(resultInt.Data == default);
             Assert.True(resultInt.IsSucceed == false);
             Assert.True(resultInt.Error != null);
             Assert.True(resultInt.Error.Message == "Two");
             Assert.True(resultInt.Error.Detail == null);
 
-            var resultCollection = await supervisor.SafeExecuteAsync<List<int>>(() => { throw new Exception("Three"); });
+            var resultCollection = await _supervisor.SafeExecuteAsync<List<int>>(() => { throw new Exception("Three"); });
             Assert.True(resultCollection.Data == default);
             Assert.True(resultCollection.IsSucceed == false);
             Assert.True(resultCollection.Error != null);
@@ -123,7 +123,7 @@ namespace Security.Tests
             Assert.True(resultCollection.Error.Detail == null);
 
 
-            var result = await supervisor.SafeExecuteAsync<int>(() => { throw new Exception("Main", new Exception("Inner")); });
+            var result = await _supervisor.SafeExecuteAsync<int>(() => { throw new Exception("Main", new Exception("Inner")); });
             Assert.True(result.Data == default);
             Assert.True(result.IsSucceed == false);
             Assert.True(result.Error != null);

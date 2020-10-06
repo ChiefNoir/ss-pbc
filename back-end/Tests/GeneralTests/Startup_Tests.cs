52a681e993584a5b2c8fbf5efef9f5c28f3b5239
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-07 00:51:28 +0300

    [back-end] Added test for Startup

---
+++ back-end/Tests/GeneralTests/Startup_Tests.cs
@@ -0,0 +1,39 @@
+﻿using API;
+using GeneralTests.Utils;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GeneralTests
+{
+    //Note: just to make sure
+    public class Startup_Tests
+    {
+        [Theory]
+        [InlineData("/api/v1/ping")]
+        public async Task Get_EndpointsReturnSuccess(string url)
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHost =>
+                {
+                    webHost.UseTestServer();
+
+                    webHost.UseConfiguration(Storage.InitConfiguration());
+                    webHost.UseStartup<Startup>();
+                });
+
+            // Build and start the IHost
+            var host = await hostBuilder.StartAsync();
+
+            var client = host.GetTestClient();
+
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+
+}

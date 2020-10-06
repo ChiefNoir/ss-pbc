0bbc7e5391b339aa85c905f67a5ce105ce7396f7
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-07 00:51:07 +0300

    [back-end] Added test api method

---
+++ back-end/API/Controllers/Public/PublicPingController.cs
@@ -0,0 +1,29 @@
+﻿using Abstractions.Supervision;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Public
+{
+    [ApiController]
+    [Route("api/v1/")]
+    public class PublicPingController : ControllerBase
+    {
+        private readonly ISupervisor _supervisor;
+
+        public PublicPingController(ISupervisor supervisor)
+        {
+            _supervisor = supervisor;
+        }
+
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            var result = _supervisor.SafeExecute
+            (
+                () => { return "pong"; } 
+            );
+
+            return new JsonResult(result);
+        }
+
+    }
+}
---
+++ back-end/Tests/GeneralTests/API/Controllers/Public/PublicPingController_Tests.cs
@@ -0,0 +1,31 @@
+﻿using Abstractions.Supervision;
+using API.Controllers.Public;
+using GeneralTests.Utils;
+using Infrastructure.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Security;
+using Xunit;
+
+namespace GeneralTests.API.Controllers.Public
+{
+    public class PublicPingController_Tests
+    {
+        [Fact]
+        internal void Ping_Test()
+        {
+            var config = Storage.InitConfiguration();
+            var log = new LogRepository(config);
+            var tokenManager = new TokenManager(config);
+            var sup = new Supervisor(log, tokenManager);
+            var api = new PublicPingController(sup);
+
+            var response = (api.Ping() as JsonResult).Value as ExecutionResult<string>;
+
+            Assert.True(response.IsSucceed);
+            Assert.Null(response.Error);
+            Assert.NotNull(response.Data);
+            Assert.Equal("pong", response.Data);
+        }
+
+    }
+}

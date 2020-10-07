64eb471046918b4483d8ee1176e9f6facafebdad
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-07 13:18:51 +0300

    [back-end] Added new HashManagerTests

--- back-end/Tests/GeneralTests/Security/HashManagerTests.cs
+++ back-end/Tests/GeneralTests/Security/HashManagerTests.cs
@@ -7,10 +7,44 @@ namespace GeneralTests.Security
     public class HashManagerTests
     {
         [Theory]
+        [InlineData("12")]
+        [InlineData("test")]
+        [InlineData("password")]
+        public void Hash_Valid(string plainText)
+        {
+            var hashManager = new HashManager();
+
+            var result = hashManager.Hash(plainText);
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.HexHash);
+            Assert.NotNull(result.HexSalt);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Hash_Invalid(string plainText)
+        {
+            var hashManager = new HashManager();
+
+            Assert.Throws<ArgumentException>(() => hashManager.Hash(plainText));
+        }
+
+        [Theory]
+        [InlineData("")]
+        public void Hash_SaltInvalid(string salt)
+        {
+            var hashManager = new HashManager();
+            Assert.Throws<ArgumentException>(() => hashManager.Hash("text", salt));
+        }
+
+
+        [Theory]
         [InlineData("password")]
         [InlineData("12")]
         [InlineData(" ")]
-        public void Hash_HexHash_MustBeDifferent(string plainText)
+        public void Hash_MustBeDifferent(string plainText)
         {
             var hashManager = new HashManager();
             
@@ -25,7 +59,7 @@ namespace GeneralTests.Security
         [InlineData(null, "salt")]
         [InlineData("", "salt")]
         [InlineData("password", "")]
-        public void Hash_HexHash_MustFail(string plainText, string salt)
+        public void Hash_MustFail(string plainText, string salt)
         {
             var hashManager = new HashManager();
 
@@ -39,7 +73,7 @@ namespace GeneralTests.Security
         [InlineData("qwerty")]
         [InlineData("EMfwzEp8jcx9sje8Fwwz")]
         [InlineData("df0af56a20f019f585d65de70c7614b75a179a180f542b102f5460b3ce8051ed")]
-        public void Hash_HexHash_Restore(string plaintText)
+        public void Hash_Restore(string plaintText)
         {
             var hashManager = new HashManager();
 

ffb10756f1c22798f68fb05fbef6aafb56ae9b34
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-13 22:57:25 +0300

    Update material.modules.ts

--- front-end/src/app/#import/material.modules.ts
+++ front-end/src/app/#import/material.modules.ts
@@ -7,7 +7,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule  } from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -15,10 +15,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-const Providers = [
-  MatDatepickerModule,
-  MatNativeDateModule
-];
+const Providers = [MatDatepickerModule, MatNativeDateModule];
 
 const Material = [
   FormsModule,
@@ -40,7 +37,6 @@ const Material = [
 @NgModule({
   imports: Material,
   exports: Material,
-  providers: Providers
+  providers: Providers,
 })
-
 export class MaterialModules {}

02f08699452b387a15c3a3c39801f35464d222da
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-20 19:46:00 +0300

    Update material.modules.ts

--- front-end/src/app/#imports/material.modules.ts
+++ front-end/src/app/#imports/material.modules.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+
+// Providers
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+//
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { TextFieldModule } from '@angular/cdk/text-field';
 
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
@@ -19,6 +23,10 @@ const Providers = [MatDatepickerModule, MatNativeDateModule];
 
 const Material = [
   FormsModule,
+  ReactiveFormsModule,
+
+  TextFieldModule,
+
   MatDatepickerModule,
   MatDialogModule,
   MatDividerModule,
@@ -30,8 +38,6 @@ const Material = [
   MatTableModule,
   MatTabsModule,
   MatToolbarModule,
-  ReactiveFormsModule,
-  TextFieldModule,
 ];
 
 @NgModule({

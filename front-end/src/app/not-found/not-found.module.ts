3d5276a0b6357314751e56c6df5cb3bf0d10107f
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-23 01:49:44 +0300

    [front-end] Cleanup 404

--- front-end/src/app/not-found/not-found.module.ts
+++ front-end/src/app/not-found/not-found.module.ts
@@ -6,7 +6,13 @@ import { NotFoundRoutingModule } from './not-found.routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, NotFoundRoutingModule, SharedModule],
-  declarations: [NotFoundComponent],
+  imports: [
+    CommonModule,
+    NotFoundRoutingModule,
+    SharedModule
+  ],
+  declarations: [
+    NotFoundComponent
+  ],
 })
 export class NotFoundModule {}

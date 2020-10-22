3046009f7173b5d295413900ee2d9fbbd18fb365
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-23 02:28:27 +0300

    [back-end] Moved ApiContracts to the separate files

---
+++ back-end/Abstractions/API/GatewayControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using Abstractions.Model.System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Abstractions.API
+{
+    [ApiController]
+    [Route("api/v1/")]
+    public abstract class GatewayControllerBase : ControllerBase
+    {
+        [HttpPost("login")]
+        public abstract Task<IActionResult> LoginAsync([FromBody] Credentials credentials);
+
+        [HttpPost("token")]
+        public abstract Task<IActionResult> ValidateAsync([FromHeader] string token);
+    }
+}

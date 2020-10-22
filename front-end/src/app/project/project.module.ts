f1cd09a9877392ee4f4ae4c8dc57602254e2ecae
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-23 01:42:25 +0300

    Update project.module.ts

--- front-end/src/app/project/project.module.ts
+++ front-end/src/app/project/project.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ProjectComponent } from './project/project.component';
 import { ProjectRoutingModule } from './project.routing.module';
+import { ProjectFullComponent } from './project-full/project-full.component';
 
 import { SharedModule } from '../shared/shared.module';
-import { ProjectFullComponent } from './project-full/project-full.component';
 import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   imports: [CommonModule, ProjectRoutingModule, SharedModule],
   declarations: [ProjectComponent, ProjectFullComponent, CarouselComponent],
 })
-export class ProjectModule { }
+export class ProjectModule {}

34c60f7dd94da7d22fd06f24e2796ccb59109535
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-29 18:50:54 +0300

    [front-end] Fixed broken imports

--- front-end/src/app/core/core.module.ts
+++ front-end/src/app/core/core.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
-import { NavigationComponent } from './navigation/navigation.component';
-import { NavigationCompactComponent } from './navigation-compact/navigation-compact.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationCompactComponent } from './components/navigation-compact/navigation-compact.component';
 
 import { SharedModule } from '../shared/shared.module';
 

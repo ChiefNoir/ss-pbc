d246a775c30bed77d5a5f943498c5348b952882e
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-29 16:25:59 +0300

    [front-end] Navigation component moved to the app root

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { SharedModule } from './shared/shared.module';
 import { OnlyIntModule } from './shared/only-int.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavigationComponent } from './shared/navigation/navigation.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { NavigationCompactComponent } from './shared/navigation-compact/navigation-compact.component';
 
 @NgModule({

c4e5a9e4011353d8364b8c5c62999299ffae1e19
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-30 00:45:07 +0300

    [front-end] Added tests support

--- front-end/src/app/core/core.module.ts
+++ front-end/src/app/core/core.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { NavigationCompactComponent } from './components/navigation-compact/navigation-compact.component';
 

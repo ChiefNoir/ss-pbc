ca141c627d273bd39f46db5c4b2f3c9ab5a2b834
Stanislav <10946721+ChiefNoir@users.noreply.github.com>
2020-10-29 20:53:50 +0300

    [front-end] Formated providedIn: 'root' in ResourcesService

--- front-end/src/app/core/services/resources.service.ts
+++ front-end/src/app/core/services/resources.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class ResourcesService {
   public LoadComplete = 'Load complete';
   public InitializationComplete = 'Initialization complete';

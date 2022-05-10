3e933a56090e9ef9b11c19de19319ddabadbe8de
Chief <ChiefWork@outlook.com>
2022-05-10 21:53:23 +0300

    Frontend / Added navigation draft

---
+++ front-end-2/src/features/navigation/navigation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../src/logo.svg';
+import './navigation.scss';
+
+function Navigation() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default Navigation;

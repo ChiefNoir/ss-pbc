53917354f2894f1679ad8474e597cb45bcd3f532
Chief <ChiefWork@outlook.com>
2022-05-16 22:25:40 +0300

    Frontend / Added env (blank)

--- front-end-2/src/features/introduction/introduction.tsx
+++ front-end-2/src/features/introduction/introduction.tsx
@@ -12,7 +12,7 @@ function Introduction() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {process.env.REACT_APP_API}
         </p>
         <a
           className="App-link"

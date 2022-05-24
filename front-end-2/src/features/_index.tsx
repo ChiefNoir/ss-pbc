355fe67c9ae64f313876354c7b8e959402e33f9a
Chief <ChiefWork@outlook.com>
2022-05-24 09:57:35 +0300

    Frontend / Added ButtonContact

--- front-end-2/src/features/_index.tsx
+++ front-end-2/src/features/_index.tsx
@@ -1,5 +1,7 @@
 import Navigation from "./ui/navigation/navigation";
 import Loader from "./ui/loader/loader";
+import ButtonContact from "./ui/buttonContact/buttonContact";
 
 export { Loader };
 export { Navigation };
+export { ButtonContact };
---
+++ front-end-2/src/features/ui/buttonContact/buttonContact.tsx
@@ -0,0 +1,14 @@
+import './buttonContact.scss';
+
+function ButtonContact(key:any, url: string, displayName: string) {
+  return (
+    <a className="button-contact"
+       rel="noreferrer noopener"
+       target="_blank"
+       key = { key } 
+       href = { url }>
+      <span> { displayName } </span>
+    </a>
+)};
+
+export default ButtonContact;

a9e9af5fcde46f399e9e12475106615fe54bf490
Chief <ChiefWork@outlook.com>
2022-05-24 08:07:37 +0300

    Frontend / Added ExternalUrl to the Introduction model

--- front-end-2/src/models/Introduction.tsx
+++ front-end-2/src/models/Introduction.tsx
@@ -1,8 +1,13 @@
+import { ExternalUrl } from './_index';
+
 type Introduction = 
 {
   title: string;
   content: string;
   posterUrl: string;
+  posterDescription: string;
+  externalUrls: Array<ExternalUrl>;
+  version: number;
 }
 
 export default Introduction;

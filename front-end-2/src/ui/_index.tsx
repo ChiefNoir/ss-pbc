f9820ab188843c0e2cd59395cdd11cfe312c21ed
Chief <ChiefWork@outlook.com>
2022-05-24 10:35:14 +0300

    Fronend / Removed old loader

--- front-end-2/src/ui/_index.tsx
+++ front-end-2/src/ui/_index.tsx
@@ -1,8 +1,5 @@
 import Footer from "./footer/footer";
-export { Footer };
-
 import Header from "./header/header";
-export { Header };
 
-import Loader from "./loader/loader";
-export { Loader };
+export { Footer };
+export { Header };

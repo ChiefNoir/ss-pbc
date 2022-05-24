ee9bb6df56b9e32a5fa0a3aaf10444081f435bc3
Chief <ChiefWork@outlook.com>
2022-05-24 06:16:20 +0300

    Fontend / Added loader component

--- front-end-2/src/features/_index.tsx
+++ front-end-2/src/features/_index.tsx
@@ -1,2 +1,5 @@
 import Navigation from "./ui/navigation/navigation";
+import Loader from "./ui/loader/loader";
+
+export { Loader };
 export { Navigation };
---
+++ front-end-2/src/features/ui/loader/loader.tsx
@@ -0,0 +1,13 @@
+import './loader.scss';
+
+function Loader() {
+  return (
+  <div className="container">
+    <div className="loading">
+      <img src="/assets/images/revolver.png" alt="Loading"/>
+    </div>
+  </div>
+  )
+};
+
+export default Loader;

01d0fb7ceb82ecd4385786fa3bbc64f46f0a6adb
Chief <ChiefWork@outlook.com>
2022-05-25 23:05:53 +0300

    Frontend / Added category navigation for CategoryButton

--- front-end-2/src/pages/ProjectsPage/features/ButtonCategory/ButtonCategoryComponent.tsx
+++ front-end-2/src/pages/ProjectsPage/features/ButtonCategory/ButtonCategoryComponent.tsx
@@ -5,7 +5,7 @@ import './ButtonCategoryComponent.scss';
 
 const ButtonCategoryComponent: FunctionComponent<{category: Category}> = (props) => {
   return (
-    <NavLink to='/projects' className="button-contact">
+    <NavLink to={'/projects/'+ props.category.code } className="button-contact">
       { props.category.displayName } [{ props.category.totalProjects }]
     </NavLink>
   );

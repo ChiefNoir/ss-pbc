ba682ff51d9f440d03e1582421f67595d29161af
Chief <ChiefWork@outlook.com>
2022-05-25 23:04:12 +0300

    Frontend / Fixed 'key' for list warning

--- front-end-2/src/pages/ProjectsPage/ProjectsPage.tsx
+++ front-end-2/src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from 'react';
-import { ProjectPreview } from '../../services/models/_index';
+import { Category, ProjectPreview } from '../../services/models/_index';
 import PublicApi from '../../services/PublicApi';
 import { Loader } from '../../ui/_index';
+import ButtonCategoryComponent from './features/ButtonCategory/ButtonCategoryComponent';
 import ProjectPreviewComponent from './features/ProjectPreview/ProjectPreview';
 import './ProjectsPage.scss';
 
 function ProjectsPage() {
   const [projects, setProjects] = useState<Array<ProjectPreview>>();
+  const [categories, setCategories] = useState<Array<Category>>();
   const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     setLoading(true);
-    var result = await PublicApi.getProjects(0, 100, undefined);
+    const projectsResponse = await PublicApi.getProjects(0, 100, undefined);
+    const categoriesResponse = await PublicApi.getCategories();
 
-    setProjects(result.data.data);
+    setProjects(projectsResponse.data.data);
+    setCategories(categoriesResponse.data.data);
+
     setLoading(false);
   };
 
@@ -26,15 +31,28 @@ function ProjectsPage() {
   else
   {
     return (
-      <div className="introduction-extension">
-              {
-                projects?.map
-                (
-                  (item: ProjectPreview) => 
-                        { return ProjectPreviewComponent({project: item}) }
-                )
-              }
-            </div>
+      <div>
+        <div className='projects-categories-filter'>
+        {          
+          categories?.filter(x => x.totalProjects > 0).map
+          (
+            (item: Category) =>
+            {
+              return <ButtonCategoryComponent key={item.id} category={item} />
+            }
+          )
+        }
+        </div>
+        {
+          projects?.map
+          (
+            (item: ProjectPreview) => 
+            {
+              return <ProjectPreviewComponent key={item.code} project={item} />
+            }
+          )
+        }
+      </div>
     );
   }
 }

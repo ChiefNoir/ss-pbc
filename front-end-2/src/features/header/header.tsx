a2579d268ccecac176307ea8a27c26e685c3a568
Chief <ChiefWork@outlook.com>
2022-05-29 14:20:46 +0300

    Front-end / Header refactoring

--- front-end-2/src/features/header/header.tsx
+++ front-end-2/src/features/header/header.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div className="header">
+    <header className="app-header">
       <Link to='/'>{process.env.REACT_APP_API_AUTH_ENDPOINT}</Link>
-    </div>
+    </header>
   );
 }
 
--- front-end-2/src/ui/index.tsx
+++ front-end-2/src/ui/index.tsx
@@ -1,10 +1,7 @@
 import Footer from "./footer/footer";
-import Header from "./header/header";
 import Navigation from "./navigation/navigation";
 import Loader from "./loader/loader";
 
-
 export { Footer };
-export { Header };
 export { Navigation };
 export { Loader };

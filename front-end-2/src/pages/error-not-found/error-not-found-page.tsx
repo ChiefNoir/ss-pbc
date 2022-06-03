88c1bf52da95f92413197a093ac9792b88faacdc
Chief <ChiefWork@outlook.com>
2022-06-03 22:22:39 +0300

    Front-end / New 404 page

--- front-end-2/src/pages/error-not-found/error-not-found-page.tsx
+++ front-end-2/src/pages/error-not-found/error-not-found-page.tsx
@@ -1,11 +1,19 @@
 import "./error-not-found-page.scss";
+import { useTranslation } from "react-i18next";
+import "../../locales/i18n";
 
 function ErrorNotFoundPage() {
+  const { t } = useTranslation();
   return (
   <div className="container-not-found">
-    <div className="neon">
-      <div className="blink3s">pa</div>ge&nbsp;<div className="blink5s">no</div>t&nbsp;found</div>
+    <div className="not-found-segment">
+      <div className="not-found-glitch">
+        <span className="not-found-glitch-symbol shallow"> {t("Error.PageNotFound")} </span>
+        <span className="not-found-glitch-symbol deep"> {t("Error.PageNotFound")} </span>
+        <span className="not-found-glitch-line"></span>
+      </div>
     </div>
+  </div>
   );
 }
 

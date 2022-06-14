bb6316b3a115511aa02b31940e5a19fdfa2027c8
Chief <ChiefWork@outlook.com>
2022-06-14 23:51:52 +0300

    Front-end / Added Incidents handling to the edit-category page

--- front-end-2/src/pages-admin/admin-categories/features/edit-category.tsx
+++ front-end-2/src/pages-admin/admin-categories/features/edit-category.tsx
@@ -1,60 +1,28 @@
 import * as React from "react";
 import { TextField, Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
-import { useEffect, useState, FunctionComponent, ChangeEvent } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import { useTranslation } from "react-i18next";
-import { Category, PrivateApi } from "../../../services";
+import { Category, Incident, PrivateApi } from "../../../services";
 import "./edit-category.scss";
 import { Loader } from "../../../ui";
 
-const EditCategoryDialog: FunctionComponent<
-{
+function EditCategoryDialog(props: {
   category: Category | null,
   isOpen: boolean,
   setOpen: React.Dispatch<React.SetStateAction<boolean>>,
   merge:(category: Category) => void,
-  remove:(category: Category) => void
-}> = (props) => {
+  remove:(category: Category) => void}) {
   const { t } = useTranslation();
-  const open = props.isOpen;
-  const setOpen = props.setOpen;
   const [category, setCategory] = useState<Category|null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [incident, setIncident] = useState<Incident|null>(null);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  async function handleDelete() {
-    setIsLoading(true);
-
-    const result = await PrivateApi.deleteCategory(category!);
-
-    if (result.data.isSucceed) {
-      props.remove(category!);
-    }
-
-    setOpen(false);
-  }
-
-  async function handleSave() {
-    setIsLoading(true);
-
-    const result = await PrivateApi.saveCategory(category!);
-
-    if (result.data.isSucceed) {
-      props.merge(result.data.data);
-      setCategory(result.data.data);
-    }
-
-    setIsLoading(false);
-  }
-
+  const open = props.isOpen;
+  const setOpen = props.setOpen;
   const descriptionElementRef = React.useRef<HTMLElement>(null);
+
   useEffect(() => {
     const fetchData = async() => {
-      setCategory(null);
-      setIsLoading(false);
-
       if (open) {
         const { current: descriptionElement } = descriptionElementRef;
         if (descriptionElement !== null) {
@@ -69,10 +37,47 @@ const EditCategoryDialog: FunctionComponent<
       }
     };
 
+    setIncident(null);
+    setCategory(null);
+    setIsLoading(false);
     fetchData();
   }, [open]);
 
-  function handleChange(e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) {
+  function closeDialog() {
+    setOpen(false);
+  };
+
+  async function deleteCategory() {
+    setIsLoading(true);
+
+    const result = await PrivateApi.deleteCategory(category!);
+
+    if (result.data.isSucceed) {
+      props.remove(category!);
+      setOpen(false);
+    } else {
+      setIncident(result.data.error);
+    }
+
+    setIsLoading(false);
+  }
+
+  async function saveCategory() {
+    setIsLoading(true);
+
+    const result = await PrivateApi.saveCategory(category!);
+
+    if (result.data.isSucceed) {
+      props.merge(result.data.data);
+      setCategory(result.data.data);
+    } else {
+      setIncident(result.data.error);
+    }
+
+    setIsLoading(false);
+  }
+
+  function handleFieldChange(e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) {
     setCategory((prevState: Category|null) => ({
       ...prevState!,
       [e.target.name]: e.target.value
@@ -80,37 +85,37 @@ const EditCategoryDialog: FunctionComponent<
   };
 
   return (
-    <div>
-      <Dialog open={open} onClose={handleClose}
-        scroll="paper"
-        fullWidth={true}
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description">
-        <DialogTitle id="scroll-dialog-title">{t("Admin.Edit") + ": " + t("Category.this")} </DialogTitle>
-        {isLoading
-        && <Loader />
-        }
+  <Dialog open={open} onClose={closeDialog}
+          scroll="paper" fullWidth={true}
+          aria-labelledby="scroll-dialog-title" aria-describedby="scroll-dialog-description">
+    <DialogTitle>
+      {t("Actions.Edit") + ": " + t("Category.this")}
+    </DialogTitle>
 
-        { !isLoading && category !== null
-        && <DialogContent dividers={true} className="container-edit-category" >
+    {isLoading && <Loader />}
 
-          <TextField fullWidth={true} value = {category!.code} label= {t("Category.Code")} variant="outlined"
-                 name="code"
-                 onChange = { handleChange }/>
-        <TextField fullWidth={true} value = {category!.displayName} label= {t("Category.DisplayName")} variant="outlined"
-                 name="displayName"
-                 onChange = { handleChange }/>
+    { incident !== null && <div className="edit-category-error"> {incident.message}</div>}
+
+    { !isLoading && category !== null
+      && <DialogContent className="container-edit-category" dividers={true}>
+          <TextField label={t("Category.Code")} value={category.code ?? ""}
+                     fullWidth={true} variant="outlined"
+                     name="code" onChange={handleFieldChange}/>
+
+          <TextField label={t("Category.DisplayName")} value={category.displayName ?? ""}
+                     fullWidth={true} variant="outlined"
+                     name="displayName" onChange={handleFieldChange}/>
         </DialogContent>
-          }
-        <DialogActions>
-          <Button onClick={handleClose}> {t("Actions.Cancel")} </Button>
-          {(!category?.isEverything && category?.id)
-          && <Button onClick={handleDelete}> {t("Actions.Delete")} </Button>
-          }
-          <Button onClick={handleSave}> {t("Actions.Save")} </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+    }
+
+    <DialogActions>
+      <Button onClick={closeDialog}> {t("Actions.Cancel")} </Button>
+      {(!category?.isEverything && category?.id)
+        && <Button onClick={deleteCategory}> {t("Actions.Delete")} </Button>
+      }
+      <Button onClick={saveCategory}> {t("Actions.Save")} </Button>
+    </DialogActions>
+  </Dialog>
   );
 };
 

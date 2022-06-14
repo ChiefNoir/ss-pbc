6bd11d46fe8c45df4858c73e0b81e8aa4a12751f
Chief <ChiefWork@outlook.com>
2022-06-15 01:16:23 +0300

    Front-end / Added incident to the admin-introduction page

--- front-end-2/src/pages-admin/admin-introduction/admin-introduction-page.tsx
+++ front-end-2/src/pages-admin/admin-introduction/admin-introduction-page.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
 import "./admin-introduction-page.scss";
-import { Loader, TabPanel } from "../../ui";
-import { PublicApi, Introduction, ExternalUrl, PrivateApi } from "../../services";
+import { ErrorComponent, Loader, TabPanel } from "../../ui";
+import { PublicApi, Introduction, ExternalUrl, PrivateApi, Incident } from "../../services";
 import { useTranslation } from "react-i18next";
 import { Button, Tab, Tabs, TextField } from "@mui/material";
 import { DataGrid, GridActionsCellItem, GridRenderCellParams, GridRowId, GridToolbarContainer } from "@mui/x-data-grid";
@@ -9,21 +9,26 @@ import { DataGrid, GridActionsCellItem, GridRenderCellParams, GridRowId, GridToo
 function AdminIntroductionPage() {
   const [introduction, setIntroduction] = useState<Introduction>(new Introduction());
   const [loading, setLoading] = useState(true);
+  const [incident, setIncident] = useState<Incident|null>(null);
   const [selectedTab, setValueSelectedTab] = React.useState(0);
   const [page, setPage] = React.useState(0);
   const { t } = useTranslation();
 
   useEffect(() => {
     const fetchIntroduction = async() => {
-      setLoading(true);
-
       const result = await PublicApi.getIntroduction();
-
-      setIntroduction(result.data.data);
-      setLoading(false);
+      if (result.data.isSucceed) {
+        setIntroduction(result.data.data);
+      } else {
+        setIncident(result.data.error);
+      }
     };
 
+    setLoading(true);
+    setIncident(null);
+
     fetchIntroduction();
+    setLoading(false);
   }, []);
 
   async function save() {
@@ -34,12 +39,20 @@ function AdminIntroductionPage() {
 
       if (newUrl.data.isSucceed) {
         introduction.posterUrl = newUrl.data.data;
+      } else {
+        setIncident(newUrl.data.error);
+        setLoading(false);
+        return;
       }
     }
 
     const result = await PrivateApi.saveIntroduction(introduction);
+    if (result.data.isSucceed) {
+      setIntroduction(result.data.data);
+    } else {
+      setIncident(result.data.error);
+    }
 
-    setIntroduction(result.data.data);
     setLoading(false);
   }
 
@@ -69,7 +82,7 @@ function AdminIntroductionPage() {
     }));
   };
 
-  function processRowUpdate(newRow: ExternalUrl, oldRow: ExternalUrl): ExternalUrl | Promise<ExternalUrl> {
+  function handleRowUpdate(newRow: ExternalUrl, oldRow: ExternalUrl): ExternalUrl | Promise<ExternalUrl> {
     setIntroduction((prevState: Introduction) => ({
       ...prevState!,
       externalUrls: introduction.externalUrls.map(x => { return (x.id === oldRow.id ? newRow : x); })
@@ -100,74 +113,66 @@ function AdminIntroductionPage() {
     );
   }
 
-  if (loading) {
-    return <Loader />;
-  } else {
-    return (
-<div className="admin-introduction-container">
-  <Tabs value={selectedTab} onChange={handleChangeTab}>
-    <Tab label={t("Introduction.Content")}/>
-    <Tab label={t("Introduction.Poster")}/>
-    <Tab label={t("Introduction.ExternalUrls")}/>
-  </Tabs>
+  if (loading) { return <Loader />; }
 
-  <TabPanel value={selectedTab} index={0}>
-    <div className="admin-introduction-content">
-    <TextField name="content" fullWidth multiline rows= {15}
-               label={t("Introduction.Content")}
-               value={introduction.content}
-               onChange={handleFieldChange}
-               />
-            </div>
-  </TabPanel>
+  return (
+  <div className="admin-introduction-container">
+    {incident && <ErrorComponent message={incident.message} detail={incident.detail}/> }
 
-  <TabPanel value={selectedTab} index={1}>
-    <div className="admin-introduction-content">
-      <img className="admin-introduction-poster"
-           src={introduction.posterPreview ?? introduction.posterUrl}/>
+    <Tabs value={selectedTab} onChange={handleChangeTab}>
+      <Tab label={t("Introduction.Content")}/>
+      <Tab label={t("Introduction.Poster")}/>
+      <Tab label={t("Introduction.ExternalUrls")}/>
+    </Tabs>
 
-      <Button component="label">
-        {t("Actions.Upload")} {t("Introduction.Poster")}
-        <input type="file" hidden onChange={handleFileSelect}/>
-      </Button>
+    <TabPanel value={selectedTab} index={0}>
+      <div className="admin-introduction-content">
+        <TextField label={t("Introduction.Content")}
+                   fullWidth multiline rows= {15}
+                   name="content"
+                   value={introduction.content}
+                   onChange={handleFieldChange}/>
+      </div>
+    </TabPanel>
 
-      <TextField value={introduction.posterUrl}
-                 label={t("Introduction.PosterUrl")}
-                 fullWidth/>
-      <TextField value={introduction.posterDescription}
-                 label={t("Introduction.PosterDescription")}
-                 fullWidth/>
-    </div>
-  </TabPanel>
+    <TabPanel value={selectedTab} index={1}>
+      <div className="admin-introduction-content">
+        <img className="admin-introduction-poster"
+             src={introduction.posterPreview ?? introduction.posterUrl}/>
 
-  <TabPanel value={selectedTab} index={2}>
-    <DataGrid style={{ width: "100%" }} autoHeight
+        <Button component="label">
+          {t("Actions.Upload")} {t("Introduction.Poster")}
+          <input type="file" hidden onChange={handleFileSelect}/>
+        </Button>
+
+        <TextField value={introduction.posterUrl} fullWidth
+                   label={t("Introduction.PosterUrl")} />
+        <TextField value={introduction.posterDescription} fullWidth
+                   label={t("Introduction.PosterDescription")} />
+      </div>
+    </TabPanel>
+
+    <TabPanel value={selectedTab} index={2}>
+      <DataGrid style={{ width: "100%" }} autoHeight
         rows={introduction.externalUrls}
-        page={page}
-        onPageChange={(newPage) => setPage(newPage)}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        page={page} onPageChange={(newPage) => setPage(newPage)}
+        pageSize={5} rowsPerPageOptions={[5]}
         experimentalFeatures={{ newEditingApi: true }}
-        processRowUpdate={processRowUpdate}
-        components={{
-          Toolbar: CustomToolbar
-        }}
+        processRowUpdate={handleRowUpdate}
+        components={{Toolbar: CustomToolbar}}
         columns={[
           { field: "displayName", flex: 0.6, headerName: t("ExternalUrl.DisplayName"), type: "string", editable: true },
-          { field: "url", flex: 1, headerName: t("ExternalUrl.Url"), type: "string", editable: true }, 
+          { field: "url", flex: 1, headerName: t("ExternalUrl.Url"), type: "string", editable: true },
           {
             field: "",
             headerName: t("Actions.this"),
             renderCell: (params: GridRenderCellParams<any, ExternalUrl, any>) => {
               return [
                 <GridActionsCellItem
-                showInMenu = {true}
-                key={`delete-button-${params.row.id}`}
-                  label= {t("Actions.Delete")}
-                  onClick={() => {
-                    deleteExternalUrl(params.row.id!);
-                  }}
-                />
+                        showInMenu={true}
+                        key={`delete-external-link-button-${params.row.id}`}
+                        label={t("Actions.Delete")}
+                        onClick={() => {deleteExternalUrl(params.row.id!);}}/>
               ];
             }
           }
@@ -178,7 +183,6 @@ function AdminIntroductionPage() {
 
   <Button onClick={save} variant="outlined"> {t("Actions.Save")} </Button>
 </div>);
-  }
 }
 
 export { AdminIntroductionPage };

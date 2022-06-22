c5bf7b8e386c4eb189dc25160f4d38da034dc93b
Chief <ChiefWork@outlook.com>
2022-06-22 20:59:28 +0300

    Back-end / Added comments to the model

--- back-end/Abstractions/Security/Credentials.cs
+++ back-end/Abstractions/Security/Credentials.cs
@@ -1,10 +1,20 @@
-﻿namespace Abstractions.Security
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Abstractions.Security
 {
+    [ExcludeFromCodeCoverage]
+    /// <summary> User credentials</summary>
     public class Credentials
     {
+        /// <summary> Get/set login </summary>
         public string Login { get; set; }
+
+        /// <summary> Get/set password </summary>
         public string Password { get; set; }
 
+        /// <summary> Create credentials</summary>
+        /// <param name="login">Login</param>
+        /// <param name="password">Password</param>
         public Credentials(string login, string password)
         {
             Login = login;

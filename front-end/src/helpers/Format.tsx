655369965de3cf8d3e4cefce9a602999bd2c29a7
Chief <ChiefWork@outlook.com>
2023-02-23 16:38:12 +0300

    frontend / fixed build error

--- front-end/src/helpers/Format.tsx
+++ front-end/src/helpers/Format.tsx
@@ -1,11 +1,9 @@
-import { useTranslation } from "react-i18next";
-import "../locales/i18n";
+import { i18n } from "../locales/i18n";
 
 class Format {
   public static ToDate(value: Date | null): string {
-    const { t } = useTranslation();
     if (value === null) {
-      return t("Project.ReleaseDateNull");
+      return i18n.t("Project.ReleaseDateNull");
     }
 
     const dt = new Date(value);
